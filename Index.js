e4d8fa1d09d009e4ea7b0a81acbbfeab4c62bf44
Jacob Werner <jacob.scott.werner@gmail.com>
2021-03-25 13:17:14 -0500

    Created compressStringOfChars function; user is prompted to enter a string of characters, it compresses like characters in order and removes duplicates to replace them with a number reprisenting the number of times in order, it then displays the compressed string. Created button to call compressStringOfChars function in Index.html.

--- Index.js
+++ Index.js
@@ -39,3 +39,32 @@ function capitalizeLetters(){
    
     displayResult(capitalizedSentence);
 }
+
+function compressStringOfChars(){
+    let userInput = prompt("Enter a string of characters you would like to compress");
+    let compressedString = "";
+    let numOfSameChar = 1
+    let lastChar;
+
+    for (let index = 0; index < userInput.length; index++) {
+        if (index == 0) {
+        } else if(userInput.length - 1 == index && userInput[index] != lastChar){
+            compressedString += userInput[index] + "1";
+
+        } else if (userInput.length - 1 == index && userInput[index] == lastChar ) {
+            numOfSameChar += 1;
+            compressedString += lastChar + numOfSameChar.toString();
+
+        } else if (userInput[index] != lastChar ){
+            compressedString += lastChar + numOfSameChar.toString();
+            numOfSameChar = 1;
+
+        } else if (userInput[index] == lastChar) {
+            numOfSameChar += 1;
+
+        } 
+        lastChar = userInput[index];
+    }
+
+    displayResult(compressedString);
+}
