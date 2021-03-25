8ba7243b3ee65171fcce0e56b87ffa0b19cf82c7
Jacob Werner <jacob.scott.werner@gmail.com>
2021-03-25 12:17:09 -0500

    Created capitilizeLetters function which prompts a user to enter a sentence, then every first letter of each word entered is capitalized, then displays the results. Created button in Index.html to call this function.

--- Index.js
+++ Index.js
@@ -7,9 +7,35 @@ function reverseString(){
     let reversedString = "";
     
     for (let index = userInput.length -1; index >= 0; index--) {
-        let inputChar = userInput[index]
-        reversedString += inputChar
+        let inputChar = userInput[index];
+        reversedString += inputChar;
     }
 
    displayResult(reversedString);
+}
+
+function capitalizeLetters(){
+    let userInput = prompt("Enter a sentence/word you would like to reverse");
+    let capitalizedSentence = "";
+    let spaceCharPassed = true
+    let firstLetter = true
+
+    for (let index = 0; index < userInput.length; index++) {
+        
+        if (userInput[index] == ' ') {
+            spaceCharPassed = true;
+            firstLetter = true;
+            capitalizedSentence += userInput[index]
+
+        } else if (spaceCharPassed == true && firstLetter == true ) {
+            firstLetter = false;
+            capitalizedSentence += userInput[index].toUpperCase();
+            
+        } else  {
+            firstLetter = false;
+            capitalizedSentence += userInput[index];
+        }
+    }
+   
+    displayResult(capitalizedSentence);
 }
